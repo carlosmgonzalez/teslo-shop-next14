01e4de9a31767014518cbcab501d9bc82bf0aa3c
Carlos Gonzalez <carlosmgonzalez1998@gmail.com>
2024-04-13 14:29:30 -0300

    Probando lazygit
    Esto es solo una prueba, add next-cookies and actios cart

---
+++ src/shopping-cart/actions.ts
@@ -0,0 +1,21 @@
+"use client"
+
+import { getCookie, hasCookie } from 'cookies-next'
+
+export const getCookieCart = (): { [id: string]: number } => {
+  if (hasCookie("cart")) {
+    const cookieCart = JSON.parse(getCookie("cart") as string ?? "{}")
+    return cookieCart
+  }
+
+  return {}
+}
+
+export const addProductToCart = (id: string) => {
+  const cookieCart = getCookieCart()
+  if (cookieCart[id]) {
+    cookieCart[id] = cookieCart[id] += 1
+  } else {
+    cookieCart[id] = 1
+  }
+}
